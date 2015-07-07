d030c4195f3664996575886a90bf6db411586cdc
Jim Wang <jinjwang@cisco.com>
2015-07-07 16:35:45 +0800

    init commit

---
+++ mmc_server/mmc_server/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mmc_server
+//
+//  Created by Jim Wang on 7/7/15.
+//  Copyright (c) 2015 cisco-webex-mac-team. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ mmc_server/mmc_server/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  mmc_server
+//
+//  Created by Jim Wang on 7/7/15.
+//  Copyright (c) 2015 cisco-webex-mac-team. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
